a28e9bc4cb92b0971bd5c5d17a69d3efc0381258
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2023-01-28 00:21:16 -0300

    Exemplo de enumeração em C#

---
+++ Aplicacao/Aplicacao/Program.cs
@@ -0,0 +1,23 @@
+﻿using Aplicacao.Entities;
+using Aplicacao.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace Aplicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+        }
+    }
+}
